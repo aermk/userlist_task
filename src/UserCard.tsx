159e523cd016a283af82c88b096178cab22c3dc1
aermk <ermakovaas7@gmail.com>
2024-10-06 20:33:32 +0300

    feat: add user list, user cards, styles, mock data, types

--- src/UserCard.tsx
+++ src/UserCard.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { FC } from "react";
+import styles from "./styles.module.scss";
 
-const UserCard = () => {
-  return <div>UserCard</div>;
+import { User as UserTypeProps } from "./types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAt,
+  faMapLocationDot,
+  faNetworkWired,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+
+const UserCard: FC<UserTypeProps> = ({
+  name,
+  email,
+  phone,
+  website,
+  address,
+}) => {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.userName}>{name}</h3>
+      <p className={styles.userEmail}>
+        <FontAwesomeIcon className={styles.icon} icon={faAt} />
+        {email}
+      </p>
+      <p className={styles.userPhone}>
+        <FontAwesomeIcon className={styles.icon} icon={faPhone} />
+        {phone}
+      </p>
+      <a
+        href={`http://${website}`}
+        className={styles.userWebsite}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon className={styles.icon} icon={faNetworkWired} />
+        {website}
+      </a>
+      <p className={styles.userAddress}>
+        <FontAwesomeIcon className={styles.icon} icon={faMapLocationDot} />
+        {`${address.city}, ${address.street}, ${address.suite}`}
+      </p>
+    </div>
+  );
 };
 
 export default UserCard;
--- src/UserList.tsx
+++ src/UserList.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { FC } from "react";
+import { User } from "./types";
+import UserCard from "./UserCard";
+import styles from "./styles.module.scss";
 
-const UserList = () => {
-  return <div>UserList</div>;
+type userListPropsType = {
+  userList: User[];
+};
+
+const UserList: FC<userListPropsType> = ({ userList }) => {
+  return (
+    <div className={styles.userListContainer}>
+      {userList.map((user, index) => (
+        <UserCard key={index} {...user}></UserCard>
+      ))}
+    </div>
+  );
 };
 
 export default UserList;
